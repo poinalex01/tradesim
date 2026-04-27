2f00ad0423b555ae68e7a209bbe2b3c923be3050
Alexander Poinstingl <150393866+poinalex01@users.noreply.github.com>
2026-04-27 13:39:27 +0200

    Initial commit

---
+++ src/main/java/com/tradesim/TradesimApplication.java
@@ -0,0 +1,13 @@
+package com.tradesim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradesim/TradesimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

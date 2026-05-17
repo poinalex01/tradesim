552de4b44a29c508e0449d4b80c3bc4393d46082
poinalex01 <poinalex01@gmail.com>
2026-05-17 19:20:26 +0200

    fixed leaderbaord if multiple players are in a lobby

--- src/main/java/com/tradesim/entity/Portfolio.java
+++ src/main/java/com/tradesim/entity/Portfolio.java
@@ -22,7 +22,7 @@ public class Portfolio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
